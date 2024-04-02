5556dad7d5473e5c97bc4a4343c187d8b2ffbcaa
AEPALO00 <palomaresv.ae@gmail.com>
2024-04-02 00:14:17 -0400

    js and fetch

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("btn_search").addEventListener("click", () => {
+        const in_search = document.getElementById("in_search").value;
+        
+        fetch("../travel_recommendation.json")
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+    });
 
+    document.getElementById("btn_clear").addEventListener("click", () => {
+        document.getElementById("in_search").value = null;
+    });
+})
